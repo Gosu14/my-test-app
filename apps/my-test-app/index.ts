8789734683c5a1ab4ac7828ebde33a0595682994
Etienne <etienne@secretarium.org>
2024-11-08 14:41:30 +0000

    feat: add echo query

--- apps/my-test-app/index.ts
+++ apps/my-test-app/index.ts
@@ -5,6 +5,14 @@ const myTableName = "my_storage_table";
 
 /**
  * @query
+ */
+export function echo(input: string): void 
+{
+    Notifier.sendString("Echo: " + input);
+}
+
+/**
+ * @query
  * @param {FetchInput} input - A parsed input argument
  */
 export function fetchValue(input: FetchInput): void {
